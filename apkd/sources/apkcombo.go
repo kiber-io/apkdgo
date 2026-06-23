5376370e7ae771e512605cf9b884f709201998c4
kiber.io <60169133+kiber-io@users.noreply.github.com>
2026-06-23 23:59:18 +0200

    Rework apkcombo version resolution and download flow

--- apkd/sources/apkcombo.go
+++ apkd/sources/apkcombo.go
@@ -3,6 +3,7 @@ package sources
 import (
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	neturl "net/url"
 	"path"
@@ -13,15 +14,23 @@ import (
 	fakeUserAgent "github.com/lib4u/fake-useragent"
 
 	"github.com/PuerkitoBio/goquery"
-	"github.com/dustin/go-humanize"
 )
 
 type ApkCombo struct {
 	BaseSource
+	baseURL string
+}
+
+type apkComboVersionItem struct {
+	VersionName string
+	Link        string
+	Type        FileType
+	VersionCode int
 }
 
 type ApkComboConfig struct {
 	BaseSourceConfig `yaml:",inline"`
+	BaseURL          string `yaml:"base_url"`
 }
 
 func (s *ApkCombo) fetchDocument(url string) (*goquery.Document, *neturl.URL, error) {
@@ -71,173 +80,244 @@ func parseVersionCodeText(rawText string) (int, error) {
 	if err != nil {
 		return 0, fmt.Errorf("invalid version code %q: %w", versionCodeText, err)
 	}
+	if versionCode <= 0 {
+		return 0, fmt.Errorf("invalid version code %q: must be positive", versionCodeText)
+	}
 	return versionCode, nil
+}
+
+func parseApkComboVersionName(rawText string) (string, error) {
+	versionNameFull := strings.TrimSpace(rawText)
+	versionParts := strings.Fields(versionNameFull)
+	if len(versionParts) < 2 {
+		return "", fmt.Errorf("invalid version name %q", versionNameFull)
+	}
+	versionName := versionParts[len(versionParts)-1]
+	if versionName == "" {
+		return "", fmt.Errorf("invalid version name %q", versionNameFull)
+	}
+	return versionName, nil
+}
+
+func parseApkComboFileType(rawText string) (FileType, error) {
+	switch strings.TrimSpace(rawText) {
+	case "APK":
+		return APK, nil
+	case "XAPK":
+		return XAPK, nil
+	default:
+		return "", fmt.Errorf("unknown file type: %q", rawText)
+	}
 }
 
 func (s *ApkCombo) Name() string {
 	return "apkcombo"
 }
+
 func (s *ApkCombo) Download(version Version) (*DownloadStream, error) {
-	req, err := s.NewRequest("GET", version.Link, nil)
+	checkin, err := s.checkin(version.Link)
+	if err != nil {
+		return nil, fmt.Errorf("failed to perform checkin: %w", err)
+	}
+	downloadLink := version.Link + "&" + checkin
+	req, err := s.NewRequest("GET", downloadLink, nil)
 	if err != nil {
 		return nil, err
 	}
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+	req = network.WithCheckRedirect(req, func(req *http.Request, via []*http.Request) error {
+		req.Header.Del("Referer")
+		return nil
+	})
 	return createResponseReader(s.Http(), req)
+}
+
+func (s *ApkCombo) parseVersionItem(e *goquery.Selection) (apkComboVersionItem, error) {
+	verNameBlock := e.Find(".vername")
+	if verNameBlock.Length() == 0 {
+		return apkComboVersionItem{}, errors.New("version name not found")
+	}
+	versionName, err := parseApkComboVersionName(verNameBlock.Text())
+	if err != nil {
+		return apkComboVersionItem{}, err
+	}
+
+	typeBlock := e.Find(".vtype")
+	if typeBlock.Length() == 0 {
+		return apkComboVersionItem{}, errors.New("file type not found")
+	}
+	fileTypeBlock := typeBlock.Children().First()
+	if fileTypeBlock.Length() == 0 {
+		return apkComboVersionItem{}, errors.New("file type not found")
+	}
+	fileType, err := parseApkComboFileType(fileTypeBlock.Text())
+	if err != nil {
+		return apkComboVersionItem{}, err
+	}
+
+	verCodeBlock := e.Find(".vercode").First()
+	if verCodeBlock.Length() == 0 {
+		return apkComboVersionItem{}, fmt.Errorf("version code not found")
+	}
+	versionCode, err := parseVersionCodeText(verCodeBlock.Text())
+	if err != nil {
+		return apkComboVersionItem{}, err
+	}
+
+	link, exists := e.Attr("href")
+	if !exists {
+		return apkComboVersionItem{}, errors.New("download link not found")
+	}
+
+	return apkComboVersionItem{
+		VersionName: versionName,
+		Link:        link,
+		Type:        fileType,
+		VersionCode: versionCode,
+	}, nil
+}
+
+func (s *ApkCombo) resolveVersionCode(versionUrl string) (apkComboVersionItem, error) {
+	doc, _, err := s.fetchDocument(versionUrl)
+	if err != nil {
+		return apkComboVersionItem{}, err
+	}
+	variantBlock := doc.Find("#variants-tab .variant")
+	if variantBlock.Length() == 0 {
+		return apkComboVersionItem{}, fmt.Errorf("no variants found for version URL %s", versionUrl)
+	}
+	versionCandidate, err := s.parseVersionItem(variantBlock)
+	if err != nil {
+		return apkComboVersionItem{}, err
+	}
+	return versionCandidate, nil
+}
+
+func (s *ApkCombo) tryToFindOldVersion(url string, versionCode int) (apkComboVersionItem, error) {
+	doc, _, err := s.fetchDocument(url)
+	if err != nil {
+		return apkComboVersionItem{}, err
+	}
+	var foundVersion apkComboVersionItem
+	doc.Find(".ver-item").EachWithBreak(func(i int, e *goquery.Selection) bool {
+		versionLink, exists := e.Attr("href")
+		if !exists {
+			s.Log().Logw("Version item missing href attribute")
+			return true
+		}
+		if versionLink == "" {
+			s.Log().Logw("Version item has empty href attribute")
+			return true
+		}
+		versionLink, err = neturl.JoinPath(s.baseURL, versionLink)
+		if err != nil {
+			s.Log().Logw(fmt.Sprintf("Failed to join version link path: %v", err))
+			return true
+		}
+		versionItem, err := s.resolveVersionCode(versionLink)
+		if err != nil {
+			s.Log().Logw(fmt.Sprintf("Failed to parse version item: %v", err))
+			return true
+		}
+		if versionItem.VersionCode != versionCode {
+			return true
+		}
+		foundVersion = versionItem
+		return false
+	})
+	if foundVersion.VersionCode == 0 {
+		return apkComboVersionItem{}, fmt.Errorf("version code %d not found", versionCode)
+	}
+	return foundVersion, nil
+}
+
+func (s *ApkCombo) checkin(referer string) (string, error) {
+	url := fmt.Sprintf("%s/checkin", s.baseURL)
+	req, err := s.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Referer", referer)
+	res, err := s.Http().Do(req)
+	if err != nil {
+		return "", fmt.Errorf("request failed: %w", err)
+	}
+	reader, err := unpackResponse(res)
+	if err != nil {
+		_ = res.Body.Close()
+		return "", err
+	}
+	defer func() {
+		_ = reader.Close()
+		if reader != res.Body {
+			_ = res.Body.Close()
+		}
+	}()
+	if res.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("error: %s", res.Status)
+	}
+	bodyBytes, err := io.ReadAll(reader)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body: %w", err)
+	}
+	return string(bodyBytes), nil
 }
 
 func (s *ApkCombo) FindByPackage(packageName string, versionCode int) (Version, error) {
 	var version Version
 
-	searchURL := "https://apkcombo.com/search?q=" + packageName
-	doc, resolvedSearchURL, err := s.fetchDocument(searchURL)
+	packageURL := fmt.Sprintf("%s/search/?q=%s", s.baseURL, packageName)
+	doc, resolvedPackageURL, err := s.fetchDocument(packageURL)
 	if err != nil {
 		return version, err
 	}
+	if resolvedPackageURL == nil {
+		return version, errors.New("failed to resolve package URL")
+	}
+	packagePageUrl := resolvedPackageURL.String()
 
-	// check that opened search page instead of app page
-	downloadIcon := doc.Find("#icon-arrow-download")
-	if downloadIcon.Length() == 0 {
-		// maybe we opened search page instead of app page, try to check the first link
-		contentBlock := doc.Find(".content")
-		if contentBlock.Length() == 0 {
-			return version, &AppNotFoundError{PackageName: packageName}
-		}
-		firstLinkBlock := contentBlock.Find(".l_item").First()
-		if firstLinkBlock.Length() == 0 {
-			return version, &AppNotFoundError{PackageName: packageName}
-		}
-		link, exists := firstLinkBlock.Attr("href")
-		if !exists {
-			return version, &AppNotFoundError{PackageName: packageName}
-		}
-		link, err = neturl.JoinPath("https://apkcombo.com", link)
+	if doc.Find(".app_header").Length() == 0 {
+		return version, &AppNotFoundError{PackageName: packageName}
+	}
+
+	var authorName string
+	authorBlock := doc.Find(".author .is-link")
+	if authorBlock.Length() == 0 {
+		s.Log().Logw(fmt.Sprintf("Author not found for package %s", packageName))
+	} else {
+		authorName = strings.TrimSpace(authorBlock.Text())
+	}
+	version.DeveloperId = authorName
+
+	// var downloadPageUrl string
+	var versionItem apkComboVersionItem
+	if versionCode != 0 {
+		oldVersionUrl, err := neturl.JoinPath(packagePageUrl, "old-versions")
 		if err != nil {
-			return version, fmt.Errorf("failed to join URL path: %w", err)
+			return version, err
 		}
-		lastPart := path.Base(link)
-		if !strings.EqualFold(lastPart, packageName) {
-			return version, &AppNotFoundError{PackageName: packageName}
+		versionItem, err = s.tryToFindOldVersion(oldVersionUrl, versionCode)
+		if err != nil {
+			return version, err
 		}
-		packageName = lastPart
-		doc, resolvedSearchURL, err = s.fetchDocument(link)
+	} else {
+		downloadPageUrl, err := neturl.JoinPath(packagePageUrl, "download/apk")
+		if err != nil {
+			return version, err
+		}
+		versionItem, err = s.resolveVersionCode(downloadPageUrl)
 		if err != nil {
 			return version, err
 		}
 	}
-
-	authorBlock := doc.Find(".author .is-link")
-	if authorBlock.Length() == 0 {
+	if versionItem.VersionCode == 0 {
 		return version, &AppNotFoundError{PackageName: packageName}
 	}
-	authorName := strings.TrimSpace(authorBlock.Text())
-
-	if resolvedSearchURL == nil {
-		return version, errors.New("failed to resolve search URL")
-	}
-	oldVersionsURL, err := neturl.JoinPath(resolvedSearchURL.String(), "old-versions")
-	if err != nil {
-		return version, fmt.Errorf("failed to join old-versions URL path: %w", err)
-	}
-	doc, resolvedOldVersionsURL, err := s.fetchDocument(oldVersionsURL)
-	if err != nil {
-		return version, err
-	}
-	if resolvedOldVersionsURL == nil {
-		return version, errors.New("failed to resolve old versions URL")
-	}
-
-	doc.Find(".ver-item").EachWithBreak(func(i int, q *goquery.Selection) bool {
-		link, exists := q.Attr("href")
-		if !exists {
-			return true
-		}
-		linkUrl, err := neturl.Parse(link)
-		if err != nil {
-			return true
-		}
-		link = resolvedOldVersionsURL.ResolveReference(linkUrl).String()
-		versionDoc, resolvedVersionURL, err := s.fetchDocument(link)
-		if err != nil {
-			return true
-		}
-		if resolvedVersionURL == nil {
-			return true
-		}
-
-		versionBlocks := versionDoc.Find(".variant")
-		if versionBlocks.Length() == 0 {
-			return true
-		}
-		versionBlock := versionBlocks.First()
-		vercodeBlock := versionBlock.Find(".vercode")
-		if vercodeBlock.Length() == 0 {
-			return true
-		}
-		versionCodeRemote, err := parseVersionCodeText(vercodeBlock.Text())
-		if err != nil {
-			return true
-		}
-		if versionCode != 0 && versionCodeRemote != versionCode {
-			return true
-		}
-		vernameBlock := versionBlock.Find(".vername")
-		if vernameBlock.Length() == 0 {
-			return true
-		}
-		versionNameFull := vernameBlock.Text()
-		versionParts := strings.Split(versionNameFull, " ")
-		if len(versionParts) < 2 {
-			return true
-		}
-		versionName := versionParts[len(versionParts)-1]
-		link, exists = versionBlock.Attr("href")
-		if !exists {
-			return true
-		}
-		linkUrl, err = neturl.Parse(link)
-		if err != nil {
-			return true
-		}
-		link = resolvedVersionURL.ResolveReference(linkUrl).String()
-
-		sizeBlock := versionBlock.Find(".description .spec.ltr")
-		if sizeBlock.Length() == 0 {
-			return true
-		}
-		sizeText := strings.TrimSpace(sizeBlock.Text())
-		size, err := humanize.ParseBytes(sizeText)
-		if err != nil {
-			return true
-		}
-		vTypeBlock := versionBlock.Find(".vtype")
-		if vTypeBlock.Length() == 0 {
-			return true
-		}
-		var fileType FileType
-		fileTypeBlock := vTypeBlock.Children()
-		if fileTypeBlock.Length() == 0 {
-			return true
-		}
-		fileTypeText := strings.TrimSpace(fileTypeBlock.Text())
-		switch fileTypeText {
-		case "APK":
-			fileType = APK
-		case "XAPK":
-			fileType = XAPK
-		}
-		if fileType == "" {
-			return true
-		}
-
-		version.Code = versionCodeRemote
-		version.Name = versionName
-		version.Link = link
-		version.Size = size
-		version.PackageName = packageName
-		version.DeveloperId = authorName
-		version.Type = fileType
-		return false
-	})
+	version.Code = versionItem.VersionCode
+	version.Name = versionItem.VersionName
+	version.Link = versionItem.Link
+	version.Type = versionItem.Type
+	version.PackageName = packageName
 
 	if version.Code == 0 {
 		return version, &AppNotFoundError{PackageName: packageName}
@@ -249,7 +329,7 @@ func (s *ApkCombo) FindByPackage(packageName string, versionCode int) (Version, 
 func (s *ApkCombo) FindByDeveloper(developerId string) ([]string, error) {
 	var packages []string
 
-	url := "https://apkcombo.com/developer/" + developerId
+	url := fmt.Sprintf("%s/developer/%s", s.baseURL, developerId)
 	req, err := s.NewRequest("GET", url, nil)
 	if err != nil {
 		return packages, err
@@ -275,7 +355,7 @@ func (s *ApkCombo) FindByDeveloper(developerId string) ([]string, error) {
 			err = errors.New("link attribute not found")
 			return false
 		}
-		link, err = neturl.JoinPath("https://apkcombo.com", link)
+		link, err = neturl.JoinPath(s.baseURL, link)
 		if err != nil {
 			err = fmt.Errorf("failed to join path: %w", err)
 			return false
@@ -291,6 +371,12 @@ func (s *ApkCombo) FindByDeveloper(developerId string) ([]string, error) {
 	return packages, err
 }
 
+func defaultApkComboConfig() ApkComboConfig {
+	return ApkComboConfig{
+		BaseURL: "https://apkcombo.com",
+	}
+}
+
 func newApkComboSource() (Source, error) {
 	s := &ApkCombo{}
 	s.Source = s
@@ -298,14 +384,23 @@ func newApkComboSource() (Source, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to create fake user agent: %w", err)
 	}
-	config, err := ResolveSourceConfig(s.Name(), ApkComboConfig{})
+	config, err := ResolveSourceConfig(s.Name(), defaultApkComboConfig())
 	if err != nil {
 		return nil, fmt.Errorf("failed to decode apkcombo config: %w", err)
 	}
+	s.baseURL = config.BaseURL
 	randomUA := ua.Filter().Platform(fakeUserAgent.Desktop).Browser(fakeUserAgent.Firefox, fakeUserAgent.Chrome).Get()
 	s.Log().Logd("Using User-Agent: " + randomUA)
 	headers := ApplyConfiguredHeaders(http.Header{
-		"User-Agent": {randomUA},
+		"User-Agent":                {randomUA},
+		"sec-gpc":                   {"1"},
+		"upgrade-insecure-requests": {"1"},
+		"sec-fetch-dest":            {"document"},
+		"sec-fetch-mode":            {"navigate"},
+		"sec-fetch-site":            {"none"},
+		"sec-fetch-user":            {"?1"},
+		"priority":                  {"u=0, i"},
+		"te":                        {"trailers"},
 	}, config.Headers)
 	s.Net = network.DefaultClientForSource(s.Name()).WithDefaultHeaders(headers)
 	return s, nil
